99c54903e2e460d46458f1faa788c62a3fefa432
aditya <146722118+AdityaRaj150@users.noreply.github.com>
2024-07-18 20:29:00 +0530

    Update Hamburger.jsx

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -3,20 +3,20 @@ export default function Hamburger({ handleHamburger, hamburgerOpen }) {
   return (
     <div
       onClick={handleHamburger}
-      className="flex flex-col gap-2 cursor-pointer items-center justify-center"
+      className="flex flex-col gap-[10px] cursor-pointer items-center justify-center"
     >
       <motion.div
-        animate={{ rotate: hamburgerOpen ? 45 : 0 }}
+        animate={{ rotate: hamburgerOpen ? 45 : 0 , y: hamburgerOpen && 10}}
         className="w-9 h-1 bg-primary dark:bg-dark-primary "
       ></motion.div>
       <motion.div
-        animate={{ opacity: hamburgerOpen ? 0 : 1, y: hamburgerOpen ? -10 : 0 }}
+        animate={{ opacity: hamburgerOpen ? 0 : 1, y: hamburgerOpen ? -10 : 5 }}
         className="w-9 h-1 bg-primary dark:bg-dark-primary "
       ></motion.div>
       <motion.div
         animate={{
           rotate: hamburgerOpen ? -45 : 0,
-          y: hamburgerOpen ? -23 : 0,
+          y: hamburgerOpen ? -13 : 0,
         }}
         className="w-9 h-1 bg-primary dark:bg-dark-primary "
       ></motion.div>
